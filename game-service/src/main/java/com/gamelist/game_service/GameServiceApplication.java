56a2bb599eb880c90f50f761553888c7d6381faf
vtran-at-speer <viet.tran@speer.io>
2024-05-27 21:54:47 -0400

    Setup database and game services

---
+++ game-service/src/main/java/com/gamelist/game_service/GameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gamelist.game_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServiceApplication.class, args);
+	}
+
+}
