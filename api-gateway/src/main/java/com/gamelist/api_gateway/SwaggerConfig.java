6793d817f60efe9724f4a0fb009ae4fc70483918
vtran-at-speer <viet.tran@speer.io>
2024-05-30 00:22:52 -0400

    Fixed format

--- api-gateway/src/main/java/com/gamelist/api_gateway/SwaggerConfig.java
+++ api-gateway/src/main/java/com/gamelist/api_gateway/SwaggerConfig.java
@@ -1,17 +1,16 @@
 package com.gamelist.api_gateway;
 
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
+
 import jakarta.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 
 @Configuration
 class SwaggerConfig {
