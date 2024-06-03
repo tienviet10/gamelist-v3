c8fbdd72cac31ffbf3d7a5b9d4606be126df486b
vtran-at-speer <viet.tran@speer.io>
2024-06-03 02:16:35 -0400

    Removed tests in game-service for now

--- game-service/src/test/java/com/gamelist/game_service/GameServiceApplicationTests.java
+++ game-service/src/test/java/com/gamelist/game_service/GameServiceApplicationTests.java
@@ -1,11 +1,6 @@
 package com.gamelist.game_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GameServiceApplicationTests {
-
-    @Test
-    void contextLoads() {}
-}
+class GameServiceApplicationTests {}
