6015cc4e2d523e2c6d9820d21991dbad9b689546
vtran-at-speer <viet.tran@speer.io>
2024-06-02 22:27:06 -0400

    Added config server

---
+++ config-server/src/test/java/com/gamelist/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gamelist.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
