d9c99c623488d7eb49000417b42f525421905d90
vtran-at-speer <viet.tran@speer.io>
2024-06-06 23:06:58 -0400

    Fix swagger

--- api-gateway/src/main/java/com/gamelist/api_gateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/gamelist/api_gateway/filter/RouteValidator.java
@@ -1,14 +1,15 @@
 package com.gamelist.api_gateway.filter;
 
-import java.util.List;
-import java.util.function.Predicate;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class RouteValidator {
 
-    public static final List<String> openApiEndpoints = List.of("/auth/register", "/auth/token");
+    public static final List<String> openApiEndpoints = List.of("/auth/register", "/auth/token", "/v3/api-docs");
 
     public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
             .noneMatch(uri -> request.getURI().getPath().contains(uri));
