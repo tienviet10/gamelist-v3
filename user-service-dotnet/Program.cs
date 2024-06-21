218d3457f7d10f10d70e21c81e191a8a72fe3b93
vtran-at-speer <viet.tran@speer.io>
2024-06-20 23:13:04 -0400

    Added user-service-dotnet deployment

--- user-service-dotnet/Program.cs
+++ user-service-dotnet/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 app.UseMetricServer();
 app.UseHttpMetrics();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
--- user-service-dotnet/config/AppDbContext.cs
+++ user-service-dotnet/config/AppDbContext.cs
@@ -10,10 +10,16 @@ namespace user_service_dotnet.config
 
     public AppDbContext(IConfiguration configuration)
     {
-      var connectionString = configuration.GetConnectionString("MongoDB");
+      var dbConnectionUrl = Environment.GetEnvironmentVariable("VAR_MONGO_DB");
+      if (string.IsNullOrEmpty(dbConnectionUrl))
+      {
+        // dbConnectionUrl = configuration.GetConnectionString("MongoDB");
+        dbConnectionUrl = configuration["ConnectionStrings:MongoDb"];
+        Console.WriteLine($"Connection string: {dbConnectionUrl}");
+      }
       var databaseName = configuration.GetConnectionString("DatabaseName");
 
-      var client = new MongoClient(connectionString);
+      var client = new MongoClient(dbConnectionUrl);
       _database = client.GetDatabase(databaseName);
     }
 
