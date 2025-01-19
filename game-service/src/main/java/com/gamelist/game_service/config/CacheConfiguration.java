9e57e1cee8d4fbb242714a67fd1e535021f7acf7
Andrew Caruso <andrew.caruso@speer.io>
2025-01-18 21:09:16 -0500

    Make game filter cache configurable

--- game-service/src/main/java/com/gamelist/game_service/config/CacheConfiguration.java
+++ game-service/src/main/java/com/gamelist/game_service/config/CacheConfiguration.java
@@ -1,6 +1,7 @@
 package com.gamelist.game_service.config;
 
 import com.github.benmanes.caffeine.cache.*;
+import org.springframework.boot.context.properties.*;
 import org.springframework.cache.*;
 import org.springframework.cache.annotation.*;
 import org.springframework.cache.caffeine.*;
@@ -10,17 +11,21 @@ import java.time.*;
 
 @EnableCaching
 @Configuration
+@ConfigurationProperties(prefix = "gamelist.game-filters")
 public class CacheConfiguration {
 
-    // TODO: Make the cache expiration time configurable
-    @Bean
-    public CacheManager gameFiltersCacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    private int expirationTime;
 
-        cacheManager.registerCustomCache("gameFilters",
-                Caffeine.newBuilder()
-                        .expireAfterWrite(Duration.ofHours(1))
-                        .build());
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        Caffeine<Object, Object> builder = Caffeine.newBuilder();
+
+        if (expirationTime > 0) {
+            builder.expireAfterWrite(Duration.ofHours(expirationTime));
+        }
+
+        cacheManager.registerCustomCache("gameFilters", builder.build());
 
         return cacheManager;
     }
